08e30fecb6372557d189912945271c46cc402375
hu.po <hugo.p.cmu@gmail.com>
2024-10-10 12:49:16 -0500

    asd

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ arc-2024 with nca
 
 - https://arcprize.org/
 - https://www.kaggle.com/competitions/arc-prize-2024
+
+- https://github.com/maxencefaldor/cax/blob/main/examples/1d_arc_nca.ipynb
--- scripts/test.oop.sh
+++ scripts/test.oop.sh
@@ -1,7 +1,7 @@
 docker run --gpus all -it --rm \
--v /home/$USER/dev/arcnca/$USER:/arcnca \
+-v /home/$USER/dev/arcnca:/arcnca \
 -v /home/$USER/dev/arcnca/data:/kaggle/input/arc-prize-2024 \
 -v /home/$USER/dev/arcnca/output:/kaggle/working \
 -e WANDB_API_KEY=$WANDB_API_KEY \
 nvcr.io/nvidia/jax:24.04-py3 \
-bash -c "pip install jupyter && jupyter nbconvert --to notebook --execute /arcnca/main.ipynb --stdout"
+bash -c "pip install jupyter && jupyter nbconvert --to notebook --execute /arcnca/test.ipynb --stdout"
