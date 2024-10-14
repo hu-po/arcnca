d44af56410393d9fb2b20db7e27672875f371e42
hu.po <hugo.p.cmu@gmail.com>
2024-10-14 09:02:32 -0500

    test conv

--- scripts/test.big.sh
+++ scripts/test.big.sh
@@ -2,6 +2,8 @@ sudo docker run --gpus all -it --rm \
 -v /home/ubuntu/arcnca:/arcnca \
 -v /home/ubuntu/arcnca/data:/kaggle/input/arc-prize-2024 \
 -v /home/ubuntu/arcnca/output:/kaggle/working \
--e MORPH_NB_FILEPATH="/arcnca/morphs/test.ipynb" \
+-e MORPH_NB_FILEPATH="/arcnca/morphs/conv.ipynb" \
+-e WANDB_ENTITY="hug" \
+-e WANDB_PROJECT="arcnca" \
 nvcr.io/nvidia/jax:24.04-py3 \
 bash -c 'pip install jupyter && jupyter nbconvert --to notebook --execute $MORPH_NB_FILEPATH --stdout'
--- scripts/test.oop.sh
+++ scripts/test.oop.sh
@@ -2,6 +2,8 @@ docker run --gpus all -it --rm \
 -v /home/$USER/dev/arcnca:/arcnca \
 -v /home/$USER/dev/arcnca/data:/kaggle/input/arc-prize-2024 \
 -v /home/$USER/dev/arcnca/output:/kaggle/working \
--e MORPH_NB_FILEPATH="/arcnca/morphs/base.ipynb" \
+-e MORPH_NB_FILEPATH="/arcnca/morphs/conv.ipynb" \
+-e WANDB_ENTITY="hug" \
+-e WANDB_PROJECT="arcnca" \
 nvcr.io/nvidia/jax:24.04-py3 \
 bash -c 'pip install jupyter && jupyter nbconvert --to notebook --execute $MORPH_NB_FILEPATH --stdout'
