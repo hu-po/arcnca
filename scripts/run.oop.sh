522d08dcb565ddbef02131250c7fe7c44413fad9
hu.po <hugo.p.cmu@gmail.com>
2024-10-14 10:24:27 -0500

    compute backend in run name

--- scripts/run.oop.sh
+++ scripts/run.oop.sh
@@ -2,6 +2,7 @@ docker run --gpus all -it --rm \
 -v /home/$USER/dev/arcnca:/arcnca \
 -v /home/$USER/dev/arcnca/data:/kaggle/input/arc-prize-2024 \
 -v /home/$USER/dev/arcnca/output:/kaggle/working \
+-e COMPUTE_BACKEND="oop" \
 -e MORPH=$MORPH \
 -e WANDB_API_KEY=$WANDB_API_KEY \
 -e WANDB_ENTITY="hug" \
--- scripts/test.ojo.sh
+++ scripts/test.ojo.sh
@@ -2,6 +2,7 @@ jetson-containers run \
 -v /home/$USER/dev/arcnca/:/arcnca \
 -v /home/$USER/dev/arcnca/data:/kaggle/input/arc-prize-2024 \
 -v /home/$USER/dev/arcnca/output:/kaggle/working \
+-e COMPUTE_BACKEND="ojo" \
 -e MORPH="conv" \
 -e WANDB_API_KEY=$WANDB_API_KEY \
 -e WANDB_ENTITY="hug" \
