a2541645725de996c3841d1f8c41dddbae44c6bb
hu.po <hugo.p.cmu@gmail.com>
2024-10-15 14:38:05 -0500

    rename to test

--- scripts/test.big.sh
+++ scripts/test.big.sh
@@ -3,7 +3,7 @@ sudo docker run --gpus all -it --rm \
 -v /home/ubuntu/arcnca/data:/kaggle/input/arc-prize-2024 \
 -v /home/ubuntu/arcnca/output:/kaggle/working \
 -e COMPUTE_BACKEND="big" \
--e MORPH="conv" \
+-e MORPH="test" \
 -e WANDB_API_KEY=$WANDB_API_KEY \
 -e WANDB_ENTITY="hug" \
 -e WANDB_PROJECT="arcnca" \
